538471fc0aecd5dc5516b8b48b5c6647905b5cd5
El_Am1go <saguilar.arana10@gmail.com>
2026-04-06 08:17:34 -0600

    Pseudocoding + FileScanner

--- Partials/Partial2/src/FileScanner.java
+++ Partials/Partial2/src/FileScanner.java
@@ -1,5 +1,14 @@
 package Partials.Partial2.src;
+import java.io.File;
+import java.util.List;
 
 public class FileScanner {
-    
+
+    public List<File> getFiles(String path) {
+        File folder = new File(path);
+        File[] files = folder.listFiles();
+        // The methood listfiles() returns an array of File objects.
+        // which are converted to a list in the following line:
+        return List.of(files);
+    }
 }
--- Partials/Partial2/src/Main.java
+++ Partials/Partial2/src/Main.java
@@ -1,5 +1,23 @@
 package Partials.Partial2.src;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        String folderPath = "Computer-Graphics/Partials/Partial2/MediaInput";
+
+        FileScanner scanner = new FileScanner();
+        MetadataExtractor extractor = new MetadataExtractor();
+        MediaSorter sorter = new MediaSorter();
+        VideoCreator creator = new VideoCreator();
+
+        // Use of "var" to infer types in Java.
+
+        // Ideal Process:
+        /* 
+        var files = scanner.getFiles(folderPath);
+        var mediaFiles = extractor.extractMetadata(files);
+        var sorted = sorter.sortByDate(mediaFiles);
+
+        creator.createVideo(sorted);
+        */
+    }
 }
