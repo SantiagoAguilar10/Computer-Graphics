bc0a5a4411f36f3cd3cde910d6b31dca7744ee9b
El_Am1go <saguilar.arana10@gmail.com>
2026-04-08 08:28:04 -0600

    Media Sorter Comparator

--- Partials/Partial2/src/MediaSorter.java
+++ Partials/Partial2/src/MediaSorter.java
@@ -1,5 +1,11 @@
 package Partials.Partial2.src;
+import java.util.Comparator;
+import java.util.List;
 
 public class MediaSorter {
-    
+
+    public List<MediaFile> sortByDate(List<MediaFile> list) {
+        list.sort(Comparator.comparing(MediaFile::getDateTaken));
+        return list;
+    }
 }
