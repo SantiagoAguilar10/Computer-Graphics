ccad0bd1a6fdea969b8e8db01be9e9dda9f22e68
El_Am1go <saguilar.arana10@gmail.com>
2026-04-08 08:18:51 -0600

    File Format

--- Partials/Partial2/src/MediaFile.java
+++ Partials/Partial2/src/MediaFile.java
@@ -1,5 +1,15 @@
 package Partials.Partial2.src;
+import java.io.File;
 
 public class MediaFile {
-    
+    private File file;
+    private String dateTaken;
+
+    public MediaFile(File file, String dateTaken) {
+        this.file = file;
+        this.dateTaken = dateTaken;
+    }
+
+    public File getFile() { return file; }
+    public String getDateTaken() { return dateTaken; }
 }
