93c5124ffbb24680eb553b9661dd890eaa934cc9
El_Am1go <saguilar.arana10@gmail.com>
2026-04-08 08:26:19 -0600

    Metadata Extractor (ExifTool)

--- Partials/Partial2/src/MetadataExtractor.java
+++ Partials/Partial2/src/MetadataExtractor.java
@@ -1,5 +1,47 @@
 package Partials.Partial2.src;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MetadataExtractor {
-    
+
+    public List<MediaFile> extractMetadata(List<File> files) {
+        List<MediaFile> result = new ArrayList<>();
+
+        for (File file : files) {
+            String date = getDateFromExif(file);
+            result.add(new MediaFile(file, date));
+        }
+
+        return result;
+    }
+
+    private String getDateFromExif(File file) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder(
+                "cmd.exe", "/c",
+                "exiftool -DateTimeOriginal \"" + file.getAbsolutePath() + "\""
+            );
+
+            Process process = pb.start();
+
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream())
+            );
+
+            String line = reader.readLine();
+
+            if (line != null) {
+                return line.split(": ", 2)[1];
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "unknown";
+    }
 }
